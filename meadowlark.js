ea9c252113cc1286ef3a92d0a7078c606df5c23f
贾森 <sen.jia@yunshipei.com>
2017-10-29 16:19:38 +0800

    添加了mocha测试

--- meadowlark.js
+++ meadowlark.js
@@ -10,12 +10,20 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    res.render('about', { number: number.getNumber() });
+    res.render('about', {
+        number: number.getNumber(),
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
 
 //定制404页面
